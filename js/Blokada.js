8aa93d008f3e4763d2748402e2026385d8777076
Arbuzio12312 <60230328+Arbuzio12312@users.noreply.github.com>
2020-02-10 21:04:49 +0100

    Update Blokada.js

--- js/Blokada.js
+++ js/Blokada.js
@@ -112,4 +112,6 @@ function avatar()
 	avatar.style.backgroundImage='url("/obrazy/user-192.png")';
 	avatar.style.opacity='1';
 	avatar.style.borderLeft=30;
+	avatar.style.position="relative";
+	avatar.style.left=40;
 }
